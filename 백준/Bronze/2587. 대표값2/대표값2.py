cfb0cad7a675fc5ee12721c65d9046332a917281
M-ingS <101794988+M-ingS@users.noreply.github.com>
2023-12-13 00:56:32 +0900

    [Bronze II] Title: 대표값2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -0,0 +1,12 @@
+k = []
+for i in range(5):
+    n = int(input())
+    k.append(n)
+s = 0
+k.sort()
+for i in k:
+    s = s + i
+avg = s / len(k)
+mid = k[len(k) // 2]    # 몫을 int로 가져옴
+print(int(avg))
+print(mid)
