406f3da082717e4f05045be395bdfa51ad6153f0
M-ingS <101794988+M-ingS@users.noreply.github.com>
2023-12-21 00:00:04 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 256 ms, Memory: 41116 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.py
@@ -3,20 +3,14 @@ input = sys.stdin.readline
 nn, m = map(int, input().split())
 n = list(map(int, input().split()))
 # n = [5 4 3 2 1]       합 배열: s = s[i-1] + n[i]
-# s = [5 9 12 14 15]    구간 합: s[j] - s[i-1]
+# s = [0 5 9 12 14 15]
 #합 배열 생성
-s = [n[0]]
-for i in range(1, len(n)):  #인덱스 1부터 5(n의길이 0~4)까지 합 배열 s를 생성
-    a = s[i-1] + n[i]
-    s.append(a)
+s = [0] * (nn+1)
+for i in range(1, nn+1):  #인덱스 1부터 5(n의길이 0~4)까지 합 배열 s를 생성
+    s[i] = s[i-1] + n[i-1]
 # print(len(s))
 #구간 합   ex) i=1 j=3
 for k in range(m):     # ex) k=0 ~ m(m=3)까지 3번 돌림. 구간 합 구하는 것을.
     i, j = map(int, input().split())
-    i = i - 1
-    j = j - 1
-    if i <= 0:
-        g = s[j]
-    else:
-        g = s[j] - s[i-1]
+    g = s[j] - s[i-1]
     print(g)
