b12cc8513418f7b0d4e524a8d1f28e82f45c71b0
M-ingS <101794988+M-ingS@users.noreply.github.com>
2024-01-03 18:42:24 +0900

    [Silver IV] Title: 덱, Time: 72 ms, Memory: 34060 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,38 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+dq = deque([])
+n = int(input())
+for i in range(n):
+    c = input().split()
+    if c[0] == 'push_front':
+        dq.appendleft(c[1])
+    elif c[0] == 'push_back':
+        dq.append(c[1])
+    elif c[0] == 'pop_front':
+        if dq:
+            print(dq.popleft())
+        else:
+            print(-1)
+    elif c[0] == 'pop_back':
+        if dq:
+            print(dq.pop())
+        else:
+            print(-1)
+    elif c[0] == 'size':
+        print(len(dq))
+    elif c[0] == 'empty':
+        if dq:
+            print(0)
+        else:
+            print(1)
+    elif c[0] == 'front':
+        if dq:
+            print(dq[0])
+        else:
+            print(-1)
+    elif c[0] == 'back':
+        if dq:
+            print(dq[-1])
+        else:
+            print(-1)
