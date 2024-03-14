ddf912c12aff85f7f3df0ef3c87d9f65df2d5435
M-ingS <101794988+M-ingS@users.noreply.github.com>
2024-03-14 23:47:22 +0900

    [Bronze III] Title: 곱셈, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+l_b = list(b)
+one = int(a) * int(l_b[2])
+two = int(a) * int(l_b[1])
+three = int(a) * int(l_b[0])
+four = int(a) * int(b)
+
+print(one)
+print(two)
+print(three)
+print(four)
