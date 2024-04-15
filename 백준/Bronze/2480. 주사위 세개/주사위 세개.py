f6d8b818d19d9689fce5004a3c07f01e91c167c0
M-ingS <101794988+M-ingS@users.noreply.github.com>
2024-04-16 00:14:53 +0900

    [Bronze IV] Title: 주사위 세개, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -2,10 +2,16 @@ a, b, c = list(map(int, input().split()))
 p = 0
 if a == b == c:
     p = 10000 + a * 1000
-elif a == c or b == c:
-    p = 1000 + c * 100
-elif a == b:
+# elif a == c or b == c:
+#     p = 1000 + c * 100
+# elif a == b:
+#     p = 1000 + a * 100
+    
+elif a == b or b == c:
+    p = 1000 + b * 100
+elif a == c:
     p = 1000 + a * 100
+    
 else:
     p = max(a, b, c) * 100
 print(p)
